7427ef9be607a0b8b0d0f44387f2c4eb0d74ad39
DylanTran5 <trandylan7@gmail.com>
2020-07-31 17:00:48 -0700

    html work initialized vars

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
-console.log("page")
+var wins = 0
+var losses = 0
+var taScore = Math.ceil(Math.random()*150 + 50) 
+var g1 = Math.ceil(Math.random()*11 + 1)
+var g2 = Math.ceil(Math.random()*11 + 1)
+var g3 = Math.ceil(Math.random()*11 + 1)
+var g4 = Math.ceil(Math.random()*11 + 1)
+var toScore = 0
+var buttonClick = function(num){
+  var gems = [gem1, gem2, gem3, gem4]
+  toScore += gems[num-1]
+  document.getElementById("totalScore").innerHTML = "Total Score: " + toScore
+}
